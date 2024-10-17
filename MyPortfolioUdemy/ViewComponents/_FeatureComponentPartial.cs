4f36307641b5dcf573ef55c3c5b0726cc9516e59
metndurn <mtndrn123@gmail.com>
2024-10-17 22:11:13 +0300

    MyPortfolio Projesinin İlk Kismi Bitti

---
+++ MyPortfolioUdemy/ViewComponents/_FeatureComponentPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+
+namespace MyPortfolioUdemy.ViewComponents
+{
+    public class _FeatureComponentPartial:ViewComponent
+    {
+        MyPortfolioContext portfolioContext=new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            /*burada sunu soylemis olduk myportfoliocontext içinde yer alan
+             *features tablosundaki butun verileri listele dedık*/
+            var values = portfolioContext.features.ToList();
+            return View(values);
+        }
+    }
+}
