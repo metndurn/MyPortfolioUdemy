0b0616514022ba787bfc95eec50083f3c72ac13f
metndurn <mtndrn123@gmail.com>
2024-12-11 21:09:09 +0300

    mesajlar kısmı halledıldı bundan once istatistikler kısmı da yapıldı

--- MyPortfolioUdemy/Controllers/MessageController.cs
+++ MyPortfolioUdemy/Controllers/MessageController.cs
@@ -25,5 +25,17 @@ namespace MyPortfolioUdemy.Controllers
 			context.SaveChanges();
 			return RedirectToAction("Inbox");
 		}
+		public IActionResult DeleteMessage(int id)//gelen mesajları silme alanıdır
+		{
+			var value = context.Messages.Find(id);
+			context.Messages.Remove(value);
+			context.SaveChanges();
+			return RedirectToAction("Inbox");
+		}
+		public IActionResult MessageDetail(int id)
+		{
+			var value = context.Messages.Find(id);
+			return View(value);
+		}
 	}
 }
